4505647022e9976b0aa4aac41011a2c4364d0a22
ferrywu <ferrywu@gmail.com>
2022-01-20 18:08:04 +0800

    Add image capture

--- app/src/main/java/com/example/cameraxtest/MainActivity.java
+++ app/src/main/java/com/example/cameraxtest/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.cameraxtest;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
@@ -14,44 +16,79 @@ import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
     private final int REQUEST_CAMERA_PERMISSION = 200;
+    private final String[] requiredPermissions = {
+            Manifest.permission.CAMERA
+    };
+
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    private ImageCapture imageCapture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         if (checkPermission()) {
             startCamera();
         }
+
+        Button captureButton = findViewById(R.id.captureButton);
+        captureButton.setOnClickListener(view -> takePicture());
     }
 
     @SuppressLint("MissingSuperCall")
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if (requestCode == REQUEST_CAMERA_PERMISSION) {
-            if ((grantResults.length > 0 ) && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+            boolean pass = (grantResults.length > 0);
+
+            for (int result : grantResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    pass = false;
+                    break;
+                }
+            }
+
+            if (pass) {
                 startCamera();
             } else {
-                Toast.makeText(this, "grant permission fail", Toast.LENGTH_LONG).show();
+                Toast.makeText(this,
+                        getResources().getString(R.string.request_permissions_fail),
+                        Toast.LENGTH_LONG).show();
+                finish();
             }
         }
     }
 
     private boolean checkPermission() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.CAMERA }, REQUEST_CAMERA_PERMISSION);
-            return false;
+        boolean pass = true;
+
+        for (String permission : requiredPermissions) {
+            if (ActivityCompat.checkSelfPermission(this,permission) != PackageManager.PERMISSION_GRANTED) {
+                pass = false;
+                break;
+            }
         }
-        return true;
+
+        if (!pass) {
+            ActivityCompat.requestPermissions(this, requiredPermissions, REQUEST_CAMERA_PERMISSION);
+        }
+
+        return pass;
     }
 
     private void startCamera() {
@@ -68,12 +105,44 @@ public class MainActivity extends AppCompatActivity {
                 PreviewView previewView = findViewById(R.id.previewView);
                 preview.setSurfaceProvider(previewView.getSurfaceProvider());
 
-                cameraProvider.bindToLifecycle(this, cameraSelector, preview);
+                imageCapture = new ImageCapture.Builder()
+                        .setTargetRotation(previewView.getDisplay().getRotation())
+                        .build();
+
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture);
             } catch (ExecutionException | InterruptedException e) {
-                Log.e(this.getClass().getName(), e.getMessage());
+                Log.e(getClass().getName(), e.getMessage());
             }
 
         }, ContextCompat.getMainExecutor(this));
     }
 
+    private void takePicture() {
+        String fileName = getExternalFilesDir("").toString() + "/image_" +
+                new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date()) + ".jpg";
+
+        ImageCapture.OutputFileOptions outputFileOptions =
+                new ImageCapture.OutputFileOptions.Builder(new File(fileName)).build();
+
+        imageCapture.takePicture(outputFileOptions,
+                ContextCompat.getMainExecutor(this),
+                new ImageCapture.OnImageSavedCallback() {
+                    @Override
+                    public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                        Toast.makeText(getBaseContext(),
+                                getResources().getString(R.string.take_picture_success),
+                                Toast.LENGTH_LONG).show();
+                        Log.d(getClass().getName(), "Saved picture to " + fileName);
+                    }
+
+                    @Override
+                    public void onError(@NonNull ImageCaptureException exception) {
+                        Toast.makeText(getBaseContext(),
+                                getResources().getString(R.string.take_picture_fail),
+                                Toast.LENGTH_LONG).show();
+                        Log.e(getClass().getName(), exception.getMessage());
+                        Log.e(getClass().getName(), "Failed to save picture to " + fileName);
+                    }
+                });
+    }
 }
