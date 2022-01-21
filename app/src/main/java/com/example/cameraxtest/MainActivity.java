f33ed3b6370e6394b687413a19e1cd9968aa1a08
ferrywu <ferrywu@gmail.com>
2022-01-21 14:57:57 +0800

    Add video capture

--- app/src/main/java/com/example/cameraxtest/MainActivity.java
+++ app/src/main/java/com/example/cameraxtest/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.cameraxtest;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageCapture;
 import androidx.camera.core.ImageCaptureException;
 import androidx.camera.core.Preview;
+import androidx.camera.core.VideoCapture;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
 import androidx.core.app.ActivityCompat;
@@ -31,11 +33,15 @@ public class MainActivity extends AppCompatActivity {
 
     private final int REQUEST_CAMERA_PERMISSION = 200;
     private final String[] requiredPermissions = {
-            Manifest.permission.CAMERA
+            Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO
     };
 
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
     private ImageCapture imageCapture;
+    private VideoCapture videoCapture;
+    private Button recordButton;
+    private boolean isRecording = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +54,15 @@ public class MainActivity extends AppCompatActivity {
 
         Button captureButton = findViewById(R.id.captureButton);
         captureButton.setOnClickListener(view -> takePicture());
+
+        recordButton = findViewById(R.id.recordButton);
+        recordButton.setOnClickListener(view -> {
+            if (!isRecording) {
+                startRecording();
+            } else {
+                stopRecording();
+            }
+        });
     }
 
     @SuppressLint("MissingSuperCall")
@@ -78,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
         boolean pass = true;
 
         for (String permission : requiredPermissions) {
-            if (ActivityCompat.checkSelfPermission(this,permission) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
                 pass = false;
                 break;
             }
@@ -91,6 +106,7 @@ public class MainActivity extends AppCompatActivity {
         return pass;
     }
 
+    @SuppressLint("RestrictedApi")
     private void startCamera() {
         cameraProviderFuture = ProcessCameraProvider.getInstance(this);
         cameraProviderFuture.addListener(() -> {
@@ -109,11 +125,15 @@ public class MainActivity extends AppCompatActivity {
                         .setTargetRotation(previewView.getDisplay().getRotation())
                         .build();
 
-                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture);
+                videoCapture = new VideoCapture.Builder()
+                        .setTargetRotation(previewView.getDisplay().getRotation())
+                        .build();
+
+                cameraProvider.unbindAll();
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, videoCapture, imageCapture);
             } catch (ExecutionException | InterruptedException e) {
                 Log.e(getClass().getName(), e.getMessage());
             }
-
         }, ContextCompat.getMainExecutor(this));
     }
 
@@ -144,5 +164,54 @@ public class MainActivity extends AppCompatActivity {
                         Log.e(getClass().getName(), "Failed to save picture to " + fileName);
                     }
                 });
+    }
+
+    private void updateUi() {
+        if (!isRecording) {
+            isRecording = true;
+            recordButton.setText(R.string.stop_button_label);
+        } else {
+            isRecording = false;
+            recordButton.setText(R.string.record_button_label);
+        }
+    }
+
+    @SuppressLint({"RestrictedApi", "MissingPermission"})
+    private void startRecording() {
+        updateUi();
+
+        String fileName = getExternalFilesDir("").toString() + "/video_" +
+                new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date()) + ".mp4";
+
+        VideoCapture.OutputFileOptions outputFileOptions =
+                new VideoCapture.OutputFileOptions.Builder(new File(fileName)).build();
+
+        videoCapture.startRecording(outputFileOptions,
+                ContextCompat.getMainExecutor(this),
+                new VideoCapture.OnVideoSavedCallback() {
+                    @Override
+                    public void onVideoSaved(@NonNull VideoCapture.OutputFileResults outputFileResults) {
+                        Toast.makeText(getBaseContext(),
+                                getResources().getString(R.string.record_video_success),
+                                Toast.LENGTH_LONG).show();
+                        Log.d(getClass().getName(), "Saved video to " + fileName);
+                        updateUi();
+                    }
+
+                    @Override
+                    public void onError(int videoCaptureError, @NonNull String message, @Nullable Throwable cause) {
+                        Toast.makeText(getBaseContext(),
+                                getResources().getString(R.string.record_video_fail),
+                                Toast.LENGTH_LONG).show();
+                        Log.e(getClass().getName(), message);
+                        Log.e(getClass().getName(), "Failed to save video to " + fileName);
+                        updateUi();
+                    }
+                });
+    }
+
+    @SuppressLint("RestrictedApi")
+    private void stopRecording() {
+        videoCapture.stopRecording();
     }
 }
